8386d8c5954ca1b61aa3adc60b88ef81ca0ea450
Mahrkeenerh <samuelbuban@gmail.com>
2021-07-07 21:33:00 +0200

    out of debug

--- F76_karma_bot.py
+++ F76_karma_bot.py
@@ -62,9 +62,8 @@ while True:
 
                 if "u/" in lowercase_body and "karma" in lowercase_body:
                     user = get_user(lowercase_body)
-                    print(comment.author)
 
-                    if user == str(comment.author):
+                    if user == "u/" + str(comment.author):
                         continue
 
                     if user not in record and "karma" in lowercase_body:
